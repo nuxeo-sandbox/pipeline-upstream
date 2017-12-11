465e2b6d98bb28b274da4457149a052f71349052
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2017-12-11 14:32:36 +0100

    filter

--- downstream.groovy
+++ downstream.groovy
@@ -1,7 +1,7 @@
 node {
 //        copyRemoteArtifacts from: 'jenkins://0e359358d5afcea47e2a002480c897d4/pipeline-upstream/master', mapper: [$class: 'FlattenDirectoriesArtifactNameMapper'], selector: [$class: 'UpstreamRemoteBuildSelector', fallbackToLastSuccessful: false, pickFirst: true]
      try {
-     copyRemoteArtifacts from: 'jenkins://0e359358d5afcea47e2a002480c897d4/downstream', mapper: [$class: 'FlattenDirectoriesArtifactNameMapper'], selector: [$class: 'UpstreamRemoteBuildSelector', fallbackToLastSuccessful: false, pickFirst: true]
+       copyRemoteArtifacts from: 'jenkins://0e359358d5afcea47e2a002480c897d4/upstream', mapper: [$class: 'FlattenDirectoriesArtifactNameMapper'], selector: [$class: 'UpstreamRemoteBuildSelector', fallbackToLastSuccessful: false, pickFirst: true]
      } catch (IOException cause) {
        echo cause
        throw cause

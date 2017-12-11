f569e345f324f21fe41ebbf3bb7498f2984193a2
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2017-12-11 11:46:08 +0100

    filter

--- downstream.groovy
+++ downstream.groovy
@@ -1,6 +1,5 @@
 node {
     echo 'hello world'
-    try {
 //        copyRemoteArtifacts from: 'jenkins://0e359358d5afcea47e2a002480c897d4/pipeline-upstream/master', mapper: [$class: 'FlattenDirectoriesArtifactNameMapper'], selector: [$class: 'UpstreamRemoteBuildSelector', fallbackToLastSuccessful: false, pickFirst: true]
         step ([$class: 'CopyArtifact',
                 projectName: 'sourceproject',
@@ -9,7 +8,4 @@ node {
                 mapper: [$class: 'FlattenDirectoriesArtifactNameMapper'],
                 selector: [$class: 'TriggeredBuildSelector', fallbackToLastSuccessful: false],
                 filter: '**/*.txt'])
-    } catch (IOException cause) {
-        echo 'artifact unavailable'
-    }
 }
